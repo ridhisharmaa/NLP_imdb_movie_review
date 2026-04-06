cf7922c9ac3aa5beedf3e159dd4c91e823c3647a
ridhi sharma <11a.ridhisharma@gmail.com>
2026-04-06 12:24:36 +0530

    Updated model / fixed preprocessing

--- main.py
+++ main.py
@@ -20,21 +20,33 @@ def decode_review(encoded_review):
   return ' '.join([reverse_word_index.get(i-3,'?') for i in encoded_review])
   
 #function to preprocess user input
+import re
+
 def preprocess_text(text):
-  words=text.lower().split()
-  encoded_review=[word_index.get(word,2)+3 for word in words]
-  padded_review=sequence.pad_sequences([encoded_review],maxlen=500)
-  return padded_review
+    import re
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+
+    words = text.split()
+
+    encoded_review = [
+        word_index.get(word, 2) 
+        if word_index.get(word, 2) < 10000 
+        else 2
+        for word in words
+    ]
+
+    return sequence.pad_sequences([encoded_review], maxlen=200)
 
 ##Step 3: Prediction function
 def predict_sentiment(review):
-  preprocessed_input=preprocess_text(review)
+    preprocessed_input = preprocess_text(review)
+    prediction = model.predict(preprocessed_input)
 
-  prediction=model.predict(preprocessed_input)
+    threshold = 0.69
+    sentiment = 'Positive' if prediction[0][0] > threshold else 'Negative'
 
-  sentiment='Positive' if prediction[0][0]>0.5 else 'Negative'
-
-  return sentiment, prediction[0][0]
+    return sentiment, prediction[0][0]
 
 
 
@@ -48,17 +60,14 @@ st.write("Enter a movie review to classify it as positive or negative.")
 user_input= st.text_area('Movie Review')
 
 if st.button('Classify'):
-  preprocessed_input=preprocess_text(user_input)
+  if user_input.strip() == "":
+    st.warning("Please enter a movie review.")
+  else:
+    sentiment, score = predict_sentiment(user_input)
 
-  #Make prediction
-  prediction=model.predict(preprocessed_input)
-  sentiment = 'Positive' if prediction[0][0]>0.5 else 'Negative'
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {score}')
 
-  #Display the result
-  st.write(f'Sentiment: {sentiment}')
-  st.write(f'Predictio Score: {prediction[0][0]}')
 
-else:
-  st.write('Please enter a movie review.')
 
 
